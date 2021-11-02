de3c109a3bd042a29c05dfabec282f2dda4bfa80
gahyunleeee <93176757+gahyunleeee@users.noreply.github.com>
2021-11-02 14:47:04 +0900

    마우스에 이끌리도록 수정

--- sketch.js
+++ sketch.js
@@ -9,8 +9,12 @@ function draw() {
   background(220);
   m.update();
   m.show();
-  
+
   if (mouseIsPressed) {
+  let mousePos = createVector (mouseX,mouseY);
+  let f = p5.Vector.sub(mousePos, m.pos); //마우스를 향하는 방향의 벡터 구하는 코드
+  f.limit(3); //너무 빨라서 리미트로 힘 크기 제한
+  m.applyForce(f);
     //make attract to mouse position
   }
 }
