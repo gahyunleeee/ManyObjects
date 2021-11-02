b7b8881cb6f589924a20d359cc6f22578dbcc339
gahyunleeee <93176757+gahyunleeee@users.noreply.github.com>
2021-11-02 15:18:32 +0900

    2개의 원이 돌도록

--- Mover.js
+++ Mover.js
@@ -4,20 +4,26 @@ class Mover {
     this.vel = createVector();
     this.acc = createVector();
     this.m = m;
-    
+
   }
-  
+
   applyForce(aForce) {
     let f = p5.Vector.div(aForce, this.m);
     this.acc.add(f);
   }
-  
+
+  arrtactTo (aPos) {
+    let f = p5.Vector.sub(aPos, this.pos); //마우스를 향하는 방향의 벡터 구하는 코드
+    f.limit(5); //너무 빨라서 리미트로 힘 크기 제한
+    this.applyForce(f);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   show() {
     fill(0);
     circle(this.pos.x, this.pos.y, this.m);
--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-let m;
+let m, m2;
 
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(300, 300, 50);
 }
 
 function draw() {
@@ -10,11 +11,21 @@ function draw() {
   m.update();
   m.show();
 
+  m2.update();
+  m2.show();
+
   if (mouseIsPressed) {
-  let mousePos = createVector (mouseX,mouseY);
-  let f = p5.Vector.sub(mousePos, m.pos); //마우스를 향하는 방향의 벡터 구하는 코드
-  f.limit(3); //너무 빨라서 리미트로 힘 크기 제한
-  m.applyForce(f);
-    //make attract to mouse position
+    let mousePos = createVector (mouseX,mouseY);
+      m.attractTo(mousePos);
+      m2.attractTo(mousePos);
+
+  // let f = p5.Vector.sub(mousePos, m.pos); //마우스를 향하는 방향의 벡터 구하는 코드
+  // f.limit(5); //너무 빨라서 리미트로 힘 크기 제한
+  // m.applyForce(f);
+  //   //make attract to mouse position
+  //
+  // let f2 = p5.Vector.sub(mousePos, m2.pos);
+  // f.limit(7);
+  // m2.applyForce(f2);
   }
 }
