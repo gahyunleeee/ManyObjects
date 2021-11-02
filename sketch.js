ce1325dea84e5bb62d033b8c82d1bd12b8b94d11
gahyunleeee <93176757+gahyunleeee@users.noreply.github.com>
2021-11-02 15:29:50 +0900

    for 반복문

--- sketch.js
+++ sketch.js
@@ -1,31 +1,39 @@
 let m, m2;
+let movers = [];
 
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
   m2 = new Mover(300, 300, 50);
+
+for (let i=0; i<5; i++) {
+  let mover = new Mover(random(400),random(400),random(5,500),) //이 변수는 이 안에서만 적용됨
+  movers.push(mover);
+}
+
 }
 
 function draw() {
   background(220);
-  m.update();
-  m.show();
+  // for (let i=0; i<movers.length; i++) {
+  //   let mover = mevers[i];
+  //   mover.upadate();
+  //   mever.show();
+  // }
+  //아래 for구문이랑 똑같은 의미임
 
-  m2.update();
-  m2.show();
+for (let mover of movers) {
+  mover.upadate();
+  mover.show();
+}
+
 
   if (mouseIsPressed) {
     let mousePos = createVector (mouseX,mouseY);
-      m.attractTo(mousePos);
-      m2.attractTo(mousePos);
+    for (let mover of movers) {
+      mover.attractTo(mousePos);
+    }
 
-  // let f = p5.Vector.sub(mousePos, m.pos); //마우스를 향하는 방향의 벡터 구하는 코드
-  // f.limit(5); //너무 빨라서 리미트로 힘 크기 제한
-  // m.applyForce(f);
-  //   //make attract to mouse position
-  //
-  // let f2 = p5.Vector.sub(mousePos, m2.pos);
-  // f.limit(7);
-  // m2.applyForce(f2);
+
   }
 }
